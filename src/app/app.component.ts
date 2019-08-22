939a12dbcd870e4bf8d2bc04c4a5a5c6cc28a399
Ronan Girault <girault.ronan@gmail.com>
2019-08-23 00:00:26 +0200

    add features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Appareil } from './models/appareil';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'projet-domotique';
+  appareil:string = '';
+
+  tv:object   = new Appareil('TV', true);
+  xbox:object = new Appareil('Xbox', true);
+  ps:object   = new Appareil('PlayStation4', false);
+  lv:object   = new Appareil('Machine à laver', true);
+
+  tabAppareil:object[] = [this.tv, this.xbox, this.ps, this.lv];
+
+  onAllumer():void {
+    for (let a of this.tabAppareil) {
+      a.status = true;
+    }
+  }
+
+  onEteindre():void {
+    for (let a of this.tabAppareil) {
+      a.status = false;
+    }
+  }
+
+  addAppareil():void {
+    let appareil:object = new Appareil(this.appareil);
+
+    if (appareil.name.trim().length > 0) {
+        this.tabAppareil.push(appareil);
+    }
+
+    this.appareil = '';
+  }
 }
+
---
+++ src/app/appareil/appareil.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from '@angular/core';
+import { Appareil } from './models/appareil';
+
+@Component({
+  selector: 'app-appareil',
+  templateUrl: './appareil.component.html',
+  styleUrls: ['./appareil.component.scss']
+})
+
+export class AppareilComponent implements OnInit {
+  danger:string  = 'list-group-item-danger';
+  success:string = 'list-group-item-success';
+  textColor:string = '';
+
+  @Input() appareilName:string;
+  @Input() appareilStatus:boolean;
+
+   getName():string{
+      return this.appareilName.toUpperCase();
+  }
+
+  getColor():string {
+    this.appareilStatus == true ? this.textColor = '' : this.textColor ='blue';
+    return this.textColor;
+  }
+
+}
